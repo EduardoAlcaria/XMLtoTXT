cd60b11431362f9ecd21abb0c9ce804319fd8ece
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-12-23 21:37:00 -0300

    Moved tagNameToPropertyName from Converter to the Format Class

--- src/service/Format.java
+++ src/service/Format.java
@@ -83,4 +83,19 @@ public class Format {
     protected static String formatPaddingRight(String str, int length) {
         return String.format("%-" + length + "s", str);
     }
+
+    protected static String tagNameToPropertyName(String tagName) {
+
+        StringBuilder result = new StringBuilder();
+        String[] parts = tagName.split("_");
+
+        for (String part : parts) {
+            if (!part.isEmpty()) {
+                result.append(part.charAt(0))
+                        .append(part.substring(1).toLowerCase());
+            }
+        }
+
+        return result.toString();
+    }
 }
