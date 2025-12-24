3a2cba171c1465ab735fd29c1aad34ed77663691
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-12-23 21:35:00 -0300

    Main class updated the show useful info for the user

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 
 import org.w3c.dom.*;
 import service.ConvertXMLToText;
+import service.IOUtils;
 
 import javax.xml.parsers.*;
 import java.io.*;
@@ -15,12 +16,16 @@ import java.util.*;
 
 public class Main {
     public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
+
+    public static int failedFiles = 0;
 
     public static void main(String[] args) throws IOException {
 
-        Path outputFile = Paths.get("entityTXTConverted");
         Path inputFolder = Paths.get("entityToBeConverted");
+        Path outputFile = Paths.get("entityTXTConverted");
 
 
         if (Files.notExists(outputFile)) {
@@ -29,7 +34,7 @@ public class Main {
 
         if (Files.notExists(inputFolder)) {
             Files.createDirectories(inputFolder);
-            System.out.println(ANSI_YELLOW + "Please put your entity files on the entityToBeConverted folder" + ANSI_RESET);
+            System.out.println(ANSI_YELLOW + "Please put your entity files on the 'entityToBeConverted' folder" + ANSI_RESET);
         }
 
 
@@ -38,10 +43,14 @@ public class Main {
                 try {
                     ConvertXMLToText.convert(path, outputFile);
                 } catch (Exception e) {
+                    failedFiles++;
                     System.out.println("error: " + e.getMessage());
-
                 }
             }
+        }
+        if (Files.size(inputFolder) > 0) {
+            System.out.println(ANSI_GREEN +  "Successes " + IOUtils.countFiles() + ANSI_RESET
+                    + ANSI_RED + "\nFailed " + failedFiles + ANSI_RESET);
         }
 
     }
